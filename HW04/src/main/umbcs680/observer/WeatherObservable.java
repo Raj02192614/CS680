e1fe57d0cb56190122514689c16448d0edbc8c58
Raj02192614 <Shivraj.gopi001@umb.edu>
2025-11-09 08:06:05 -0500

    Delete HW04 directory

--- HW04/src/main/umbcs680/observer/WeatherObservable.java
+++
@@ -1,7 +0,0 @@
-package umbcs680.observer;
-
-public class WeatherObservable extends Observable<WeatherData> {
-    public void updateWeatherData(WeatherData data) {
-        this.notifyObservers(data);
-    }
-}
