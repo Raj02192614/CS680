a03e3923ad28b845a104ab428a95e20ee3ddc758
Raj02192614 <Shivraj.gopi001@umb.edu>
2025-11-09 08:05:54 -0500

    Delete HW03 directory

--- HW03/src/main/umbcs680/order/OrderState.java
+++
@@ -1,8 +0,0 @@
-package umbcs680.order;
-
-public interface OrderState {
-    void shipOrder(OrderContext context);
-    void deliverOrder(OrderContext context);
-    void returnOrder(OrderContext context);
-    void refundOrder(OrderContext context);
-}
